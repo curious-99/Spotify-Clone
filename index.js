a1c6665cef5a02ce0255532345d97931f057d93e
Mohit <mohit39.ugcs20@iiitranchi.ac.in>
2023-02-10 22:13:31 +0530

    some what finish

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay= document.getElementById('masterPlay');
 let myProgreebar= document.getElementById('myProgreebar');
+let masterSongName= document.getElementById('masterSongName');
 let gif= document.getElementById('gif');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 // audioElement.play();
@@ -70,7 +71,39 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click',(e)=>{
         // console.log(e.target);
         makeAllPlays();
+        songIndex = parseInt(e.target.id);
         e.target.classList.remove('fa-circle-play');
         e.target.classList.add('fa-circle-pause');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime=0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
     })
+})
+
+document.getElementById('next').addEventListener('click',()=>{
+    if(songIndex>=9) songIndex=0;
+    else songIndex += 1;
+
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
+document.getElementById('previous').addEventListener('click',()=>{
+    if(songIndex<=0) songIndex=9;
+    else songIndex -= 1;
+
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
 })
